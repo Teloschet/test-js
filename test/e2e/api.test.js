2565fdfbdecf72c1aee4d08a2a1ba4dea5bb2393
Teloschet <chrisaleiro@gmail.com>
2023-08-12 17:19:18 -0300

    test: implement _globalToken and test successfully

--- test/e2e/api.test.js
+++ test/e2e/api.test.js
@@ -1,10 +1,11 @@
 import { describe, it, before, after } from 'node:test'
-import assert from 'node:assert'
 import { app } from '../../api.js'
+import assert from 'node:assert'
 
 describe('#API E2E test Suite', () => {
   let BASE_URL = ''
   let _server = {}
+  let _globalToken = ''
   
   before(async () => {
     _server = app
@@ -21,7 +22,7 @@ describe('#API E2E test Suite', () => {
 
   after((done) => _server.close(done))
 
-  describe('login', () => {
+  describe('/login', () => {
     it('should receive not authorize when user or password is invalid', async () => {
       const input = {
         user: 'invalid',
@@ -43,8 +44,85 @@ describe('#API E2E test Suite', () => {
 
       const expectedBody = { error: 'user invalid!' }
       const response = await result.json()
-      
+
       assert.deepStrictEqual(response, expectedBody, `response.body should be ${JSON.stringify(expectedBody)}, actual: ${JSON.stringify(response)}`)
+    })
+
+    it('should login successfully given user and password', async () => {
+      const input = {
+        user: 'teloschet',
+        password: '123'
+      }
+
+      const result = await fetch(`${BASE_URL}/login`, {
+        method: 'POST',
+        body: JSON.stringify(input),
+      })
+
+      const expected = 200
+
+      assert.strictEqual(
+        result.status, 
+        expected, 
+        `status code should be 200, actul: ${result.status}`
+      )
+
+      const response = await result.json()
+      assert.ok(response.token.length > 20, `response.token should be a valid jwt token. actual: ${JSON.stringify(response)}`)
+
+      _globalToken = response.token
+    })
+  })
+
+  describe('/', () => {
+    it('should not be allowed to access private data without a token', async () => {
+      const input = {
+        headers: {
+          authorization: ''
+        }
+      }
+
+      const result = await fetch(`${BASE_URL}/`, {
+        method: 'GET',
+        headers: input.headers
+      })
+
+      const expected = 400
+
+      assert.strictEqual(
+        result.status, 
+        expected, 
+        `status code should be 400, actul: ${result.status}`
+      )
+
+      const expectedBody = { error: 'invalid token!' }
+      const response = await result.json()
+      assert.deepStrictEqual(response, expectedBody,`response.body should be ${JSON.stringify(expectedBody)}, actual: ${JSON.stringify(response)}`)
+    })
+
+    it('should be allowed to access private data with valid token', async () => {
+      const input = {
+        headers: {
+          authorization: _globalToken
+        }
+      }
+
+      const result = await fetch(`${BASE_URL}/`, {
+        method: 'GET',
+        headers: input.headers
+      })
+
+      const expected = 200
+
+      assert.strictEqual(
+        result.status, 
+        expected, 
+        `status code should be 200, actul: ${result.status}`
+      )
+
+      const expectedBody = { result: 'Hey welcome!' }
+      const response = await result.json()
+      assert.deepStrictEqual(response, expectedBody,`response.body should be ${JSON.stringify(expectedBody)}, actual: ${JSON.stringify(response)}`)
     })
   })
 })
